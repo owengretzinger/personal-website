98dd137ebdb6cc699856c26c3d4d0a0304ff747a
owengretz <otgretz@gmail.com>
2023-05-18 21:13:16 -0400

    fix loading animation bug

--- personal-website/src/components/layout.tsx
+++ personal-website/src/components/layout.tsx
@@ -28,16 +28,24 @@ export default function Layout({ bgClass = `bg-white ${animations.changeColOnSta
           <meta name="description" content="Owen Gretzinger's Personal Website" />
           <link rel="icon" href="/personal-website-icon-blue.ico" />
         </Head>
-        {!disableLoadingAnimationForTesting && 
+        {!disableLoadingAnimationForTesting && !isVisible &&
           <div className="fixed w-screen max-w-full h-screen z-[200] flex justify-center items-center pointer-events-none">
             <svg className="fixed w-full h-full">
-              <rect className={`w-full h-full ${animations.revealPage}`} fill="rgb(255,255,255)" />
+              <rect className={`w-full h-full ${animations.revealPage}`} fill="white" />
             </svg>
             <div className="w-[1000vw] h-[1000vh] flex justify-center items-center">
               <svg width="300" height="300" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${animations.scaleOut} z-[199]`}>
                 <circle cx="35" cy="35" r="31" strokeWidth="7" stroke="rgb(239,89,34)" className={animations.animateCircle} />
-                <circle cx="35" cy="35" r="28" strokeWidth="7" stroke="rgb(14,45,49)" className={`${animations.bulge} ${animations.second}`} />
-                <circle cx="35" cy="35" r="13" strokeWidth="7" stroke="rgb(14,45,49)" className={`${animations.bulge} ${animations.first}`} />
+                {/* <circle cx="35" cy="35" r="28" strokeWidth="7" stroke="rgb(14,45,49)" className={`${animations.bulge} ${animations.second}`} /> */}
+                {/* <circle cx="35" cy="35" r="13" strokeWidth="7" stroke="rgb(14,45,49)" /> */}
+
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="63" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${animations.bulge} ${animations.second} ${animations.sw63}`} />
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="49" stroke="white" strokeLinecap='round' className={`${animations.bulge} ${animations.second} ${animations.sw49}`} />
+
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="33" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${animations.bulge} ${animations.first} ${animations.sw33}`} />
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="19" stroke="white" strokeLinecap='round' className={`${animations.bulge} ${animations.first} ${animations.sw19}`} />
+
+                
               </svg>
             </div>
           </div>}
