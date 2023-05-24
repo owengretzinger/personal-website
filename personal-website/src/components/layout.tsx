1db155d5ecc90902a0500d80d90792615c01503c
owengretz <otgretz@gmail.com>
2023-05-24 19:48:38 -0400

    attempt fix loading animation bug
    attempt fix for bug where loading animation wouldn't play when navigating using the browser back/forward buttons on mobile

--- personal-website/src/components/layout.tsx
+++ personal-website/src/components/layout.tsx
@@ -6,8 +6,6 @@ import animations from './loadingAnimation.module.css';
 import { loadingAnimationPlayingContext } from '@/pages/_app';
 
 
-
-
 export default function Layout({ bgClass = `bg-white ${animations.changeColOnStart} ${animations.toBg}`, ...props }) {
   const router = useRouter();
   const linkedSection = router.query.section ? router.query.section as string : null;
@@ -23,7 +21,6 @@ export default function Layout({ bgClass = `bg-white ${animations.changeColOnSta
     }, 3800);
   }, []);
 
-
   return (
     <>
       <div className="text-black font-noto selection:bg-orange/20 relative">
@@ -32,24 +29,20 @@ export default function Layout({ bgClass = `bg-white ${animations.changeColOnSta
           <meta name="description" content="Owen Gretzinger's Personal Website" />
           <link rel="icon" href="/personal-website-icon-blue.ico" />
         </Head>
-        {
+        {showingLoadingAnimation &&
           <div className={`fixed w-screen max-w-full h-screen z-50 flex justify-center items-center ${showingLoadingAnimation ? "pointer-events-auto" : "pointer-events-none"}`}>
             <svg className="fixed w-full h-full">
-              <rect className={`w-full h-full ${animations.revealPage}`} fill="white" />
+              <rect className={`w-full h-full`} fill="white" />
             </svg>
             <div className="w-[1000vw] h-[1000vh] flex justify-center items-center">
               <svg width="300" height="300" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${animations.scaleOut} z-[199]`}>
-                <circle cx="35" cy="35" r="31" strokeWidth="7" stroke="rgb(239,89,34)" className={`${showingLoadingAnimation ? "block" : "hidden"} ${animations.animateCircle}`} />
-                {/* <circle cx="35" cy="35" r="28" strokeWidth="7" stroke="rgb(14,45,49)" className={`${animations.bulge} ${animations.second}`} /> */}
-                {/* <circle cx="35" cy="35" r="13" strokeWidth="7" stroke="rgb(14,45,49)" /> */}
+                <circle cx="35" cy="35" r="31" strokeWidth="7" stroke="rgb(239,89,34)" className={`${animations.animateCircle}`} />
 
-                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="63" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${showingLoadingAnimation ? "block" : "hidden"} ${animations.bulge} ${animations.second} ${animations.sw63}`} />
-                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="49" stroke="white" strokeLinecap='round' className={`${showingLoadingAnimation ? "block" : "hidden"} ${animations.bulge} ${animations.second} ${animations.sw49}`} />
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="63" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${animations.bulge} ${animations.second} ${animations.sw63}`} />
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="49" stroke="white" strokeLinecap='round' className={`${animations.bulge} ${animations.second} ${animations.sw49}`} />
 
-                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="33" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${showingLoadingAnimation ? "block" : "hidden"} ${animations.bulge} ${animations.first} ${animations.sw33}`} />
-                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="19" stroke="white" strokeLinecap='round' className={`${showingLoadingAnimation ? "block" : "hidden"} ${animations.bulge} ${animations.first} ${animations.sw19}`} />
-
-
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="33" stroke="rgb(14,45,49)" strokeLinecap='round' className={`${animations.bulge} ${animations.first} ${animations.sw33}`} />
+                <line x1="35" y1="35" x2="35" y2="35" strokeWidth="19" stroke="white" strokeLinecap='round' className={`${animations.bulge} ${animations.first} ${animations.sw19}`} />
               </svg>
             </div>
           </div>}
@@ -62,7 +55,5 @@ export default function Layout({ bgClass = `bg-white ${animations.changeColOnSta
         </>}
       </div>
     </>
-
-
   );
 }
