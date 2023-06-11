c02ce5c78f718935769806ad63b5229b1718faa5
owengretz <otgretz@gmail.com>
2023-06-10 21:43:56 -0400

    create sitemap

---
+++ personal-website/src/app/sitemap.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { MetadataRoute } from 'next'
+import path from 'path';
+import fs from 'fs';
+
+const articleDirectory = path.join(process.cwd(), 'public/articles');
+
+export default function sitemap(): MetadataRoute.Sitemap {
+
+    const allArticles = fs.readdirSync(path.resolve(articleDirectory, "displayed_articles")).concat(fs.readdirSync(path.resolve(articleDirectory, "hidden_articles")));
+    
+    const allArticlesSiteMap = allArticles.map((url: string) => ({
+        url: `https://owengretzinger.com/articles/${url}`,
+    }));
+
+    return [
+        {
+            url: 'https://owengretzinger.com',
+        },
+        {
+            url: 'https://owengretzinger.com/resume',
+        },
+    ].concat(allArticlesSiteMap);
+}
