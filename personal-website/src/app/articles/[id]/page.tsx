e9cdf915ef3880fb7525d8dd13409616b433e76f
owengretz <otgretz@gmail.com>
2023-06-10 21:44:33 -0400

    article metadata

--- personal-website/src/app/articles/[id]/page.tsx
+++ personal-website/src/app/articles/[id]/page.tsx
@@ -2,8 +2,32 @@ import Article from "@/components/article"
 import fs, { existsSync } from 'fs';
 import path from 'path'
 
+import { Metadata, ResolvingMetadata } from 'next'
+import matter from "gray-matter";
+
 const articleDirectory = path.join(process.cwd(), 'public/articles');
 
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ params }: Props, parent?: ResolvingMetadata): Promise<Metadata> {
+    
+    const fileName = params.id.endsWith(".md") ? params.id : `${params.id}.md`;
+
+    const pathIfDisplayed = path.resolve(articleDirectory, "displayed_articles", fileName);
+    const pathIfHidden = path.resolve(articleDirectory, "hidden_articles", fileName);
+    const pathToArticle = existsSync(pathIfDisplayed) ? pathIfDisplayed : pathIfHidden
+    const article = fs.readFileSync(pathToArticle, 'utf8');
+    const articleMatter = matter(article);
+
+    return {
+        title: `${articleMatter.data.title}: ${articleMatter.data.subtitle}`,
+        description: `${articleMatter.content.slice(0, 200)}`,
+    }
+}
 
 export default function ArticlePage({ params }: { params: { id: string } }) {
 
