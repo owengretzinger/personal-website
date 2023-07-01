1ddf7a0f8612001b4939f7df6b50b48b6284a17c
owengretz <otgretz@gmail.com>
2023-07-01 19:52:11 -0400

    add rewrites

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      { "source": "/education-data-for-change/", "destination": "https://education-data-for-change.vercel.app/" },
+      { "source": "/education-data-for-change/:match*", "destination": "https://education-data-for-change.vercel.app/:match*" },
+      { "source": "/resume-template/", "destination": "https://html-resume-template-gamma.vercel.app/" },
+      { "source": "/resume-template/:match*", "destination": "https://html-resume-template-gamma.vercel.app/:match*" },
+    ]
+  },
 }
 
 module.exports = nextConfig
