7746581359f1ba24ea42b7664cd11c5c54ea8914
owengretz <otgretz@gmail.com>
2023-07-01 15:17:49 -0400

    Update next.config.js

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/resume-template",
+          destination: "https://html-resume-template-gamma.vercel.app/",
+        },
+        {
+          source: "/resume-template/:path*",
+          destination: "https://html-resume-template-gamma.vercel.app/:path*",
+        },
+      ],
+    };
+  },
 }
 
 module.exports = nextConfig
