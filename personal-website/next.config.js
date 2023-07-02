4b8f7f17cb22d5f9a92dd609d94debcc2e37a35d
owengretz <otgretz@gmail.com>
2023-07-01 20:10:02 -0400

    Update next.config.js

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -3,17 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      { "source": "/education-data-for-change/", "destination": "https://education-data-for-change.vercel.app/" },
-      { "source": "/education-data-for-change/:match*", "destination": "https://education-data-for-change.vercel.app/:match*" },
-      { "source": "/resume-template/", "destination": "https://html-resume-template-gamma.vercel.app/" },
-      { "source": "/resume-template/:match*", "destination": "https://html-resume-template-gamma.vercel.app/:match*" },
+      { source: "/education-data-for-change/", destination: "https://education-data-for-change.vercel.app/" },
+      { source: "/education-data-for-change/:match*", destination: "https://education-data-for-change.vercel.app/:match*" },
+      { source: "/resume-template/", destination: "https://html-resume-template-gamma.vercel.app/" },
+      { source: "/resume-template/:match*", destination: "https://html-resume-template-gamma.vercel.app/:match*" },
     ]
   },
   async redirects() {
     return [
       { source: "/education-data-for-change", destination: "/education-data-for-change/", permanent: true },
       { source: "/resume-template", destination: "/resume-template/", permanent: true },
-      { source: "/:match*/", destination: "/:match*", permanent: true },
+      { source: "/(.+)/", destination: "/$1", permanent: true },
     ]
   },
 }
