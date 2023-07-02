c08988c161cfd5cbec93f03935f5b46668859325
owengretz <otgretz@gmail.com>
2023-07-01 22:40:15 -0400

    Update next.config.js

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   async rewrites() {
     return [
       { source: "/education-data-for-change", destination: "https://education-data-for-change.vercel.app" },
-      { source: "/dist/:match*", destination: "https://education-data-for-change.vercel.app/dist/:match*" },
+      { source: "/dist/output.css", destination: "https://education-data-for-change.vercel.app/dist/output.css" },
+      { source: "/script.js", destination: "https://education-data-for-change.vercel.app/script.js" },
+      { source: "/media/:path*", destination: "https://education-data-for-change.vercel.app/media/:path*" },
       { source: "/resume-template", destination: "https://html-resume-template-gamma.vercel.app" },
       { source: "/assets/:match*", destination: "https://html-resume-template-gamma.vercel.app/assets/:match*" },
     ]
