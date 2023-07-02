11f388e8259f681670598e0201eb66d64de93a3a
owengretz <otgretz@gmail.com>
2023-07-01 20:37:08 -0400

    Update next.config.js

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       { source: "/education-data-for-change", destination: "/education-data-for-change/", permanent: true },
       { source: "/resume-template", destination: "/resume-template/", permanent: true },
-      { source: "/((?!resume-template)(?!education-data-for-change).+)/", destination: "/$1", permanent: true },
+      { source: "/((?!resume-template)(?!education-data-for-change).+)\/", destination: "/$1", permanent: true },
     ]
   },
 }
